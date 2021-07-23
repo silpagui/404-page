8c8ee6135e047a5f17379250abd2f9ca88b739c2
silpagui <silpagui.sa@gmail.com>
2021-07-23 15:38:14 +0200

    ref(scss/html): aply text and color to footer

--- src/components/Copyright/Copyright.component.js
+++ src/components/Copyright/Copyright.component.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Copyright.styles.scss";
 
 export function Copyright() {
-  return <p className="copyright-text">Your name @ DevChallenges.io</p>;
+  return (
+    <p className="copyright-text">
+      created by <a href="index.html">username</a> - devChallenges.io
+    </p>
+  );
 }
