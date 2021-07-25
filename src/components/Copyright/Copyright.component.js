5008cd6a9f690d368e6ee22581be3d5380957555
silpagui <silpagui.sa@gmail.com>
2021-07-25 19:33:56 +0200

    feat(Copyright.component.js): add github username

--- src/components/Copyright/Copyright.component.js
+++ src/components/Copyright/Copyright.component.js
@@ -4,7 +4,8 @@ import "./Copyright.styles.scss";
 export function Copyright() {
   return (
     <p className="copyright-text">
-      created by <a href="index.html">username</a> - devChallenges.io
+      created by <a href="https://github.com/silpagui">silpagui</a> -
+      devChallenges.io
     </p>
   );
 }
