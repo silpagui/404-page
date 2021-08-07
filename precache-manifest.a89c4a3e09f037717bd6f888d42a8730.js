3cbef584cd1a4e35308a92da0e830143211160ed
silpagui <silpagui.sa@gmail.com>
2021-08-07 18:36:43 +0200

    Updates

---
+++ precache-manifest.a89c4a3e09f037717bd6f888d42a8730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f39c5206cd7e4e922bc93715a2a778",
+    "url": "/404-page/index.html"
+  },
+  {
+    "revision": "5eae52234d00f7c55d4d",
+    "url": "/404-page/static/css/main.5053c505.chunk.css"
+  },
+  {
+    "revision": "e07fbf45f316a72b05e7",
+    "url": "/404-page/static/js/2.27158aa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404-page/static/js/2.27158aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eae52234d00f7c55d4d",
+    "url": "/404-page/static/js/main.d931e3f0.chunk.js"
+  },
+  {
+    "revision": "58bf4e680847a144c570",
+    "url": "/404-page/static/js/runtime-main.b7be6d11.js"
+  },
+  {
+    "revision": "3ea4c98522374b11da5c876b32f4becd",
+    "url": "/404-page/static/media/scarecrow.3ea4c985.png"
+  }
+]);
